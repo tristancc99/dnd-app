6f7b42b5c0a5c56c28b4c4d64642edb3e2ea6f69
Judas <pleasedontforgetme99@gmail.com>
2020-01-16 18:34:09 -0500

    Updates

---
+++ precache-manifest.98a0fb3fb94fbf8e8eacf1b5fbc75415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64aa71f62a8715b63d20bcbd886cf0d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf0fb3e4404483982fe",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c7f461c45e58789a4910",
+    "url": "/static/css/main.384f12ce.chunk.css"
+  },
+  {
+    "revision": "2cf0fb3e4404483982fe",
+    "url": "/static/js/2.0e01fd2f.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/static/js/2.0e01fd2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7f461c45e58789a4910",
+    "url": "/static/js/main.a2da59a6.chunk.js"
+  },
+  {
+    "revision": "5231cbeb282d48c58237",
+    "url": "/static/js/runtime-main.40817292.js"
+  }
+]);
