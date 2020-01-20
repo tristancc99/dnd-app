e43c2a9291638d13485abe9195ca67445e78365a
Travis King <travisking@US-EP-ML011.local>
2020-01-20 15:13:28 -0500

    Added Extensive comments about initiativeTracker

--- src/Components/InitiativeTracker/index.js
+++ src/Components/InitiativeTracker/index.js
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 import InitForm from "./InitForm";
 import Trackee from "./Trackee";
+// Love that your trying to use this, it's very good practice
 import { array } from "prop-types";
 
 function InititativeTracker() {
   let [trackers, setTrackers] = useState([]);
-
-  // Inline this :) 
-  function compare(a, b) {
-    if (a.init > b.init) return -1;
-    if (b.init > a.init) return 1;
-  }
 
   function newTrackee(str, num, ty, tid) {
     let trackee = {
@@ -24,19 +19,38 @@ function InititativeTracker() {
 
   function removeTrackee(id) {
     let temp = [...trackers];
-    let find = temp.find(trackee => trackee.id == id);
+    let find = temp.find(trackee => trackee.id === id);
     let index = temp.indexOf(find);
     temp.splice(index, 1);
     setTrackers(temp);
   }
 
+  /* 
+    Because of the way that this works 
+    (a, b) => ???????
+      From W3 Schools
+      If the result is negative a is sorted before b.
+      If the result is positive b is sorted before a.
+      If the result is 0 no changes are done with the sort order of the two values.
+
+    I want b to be first if it has a higher initiative than a.
+      Therefore if b.init is < a.init I want a.init to go first. So based off of these rules up top
+      I would do b.init - a.init (b = 1, a = 20) result = 19 (a is sorted before b)
+
+    Also as a side note I would consider changing the naming convention of a.init. Init has it's own
+    connotation that kind of conflicts with what your trying to say. A lot of time init is short for 
+    initialize which is something you have to do to make sure everything works. To avoid problems like
+    this I would make it a good practice to overdo it on naming something, eventually you'll find the
+    happy medium :)
+
+  */
   return (
     <div>
       <div className="header">Inititative Tracker</div>
       <InitForm data={trackers} create={newTrackee} />
       <hr></hr>
       <ul>
-        {trackers.sort(compare).map(tracke => {
+        {trackers.sort((a, b) => b.init - a.init).map(tracke => {
           return (
             <Trackee remove={removeTrackee} key={tracke.id} data={tracke} />
           );
